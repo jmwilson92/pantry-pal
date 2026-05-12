dda076a6bab318ebb11e2e6abc9a3304a0f07691
jmwilson92 <44003297+jmwilson92@users.noreply.github.com>
2026-05-11 19:08:14 -0700

    Wrap app with GestureHandlerRootView to fix Swipeable error

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import HomeScreen from './screens/HomeScreen';
 import ScanScreen from './screens/ScanScreen';
 import InventoryScreen from './screens/InventoryScreen';
@@ -9,24 +10,26 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="Scan" 
-          component={ScanScreen} 
-          options={{ title: 'Scan Barcode' }} 
-        />
-        <Stack.Screen 
-          name="Inventory" 
-          component={InventoryScreen} 
-          options={{ title: 'My Inventory' }} 
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen 
+            name="Home" 
+            component={HomeScreen} 
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen 
+            name="Scan" 
+            component={ScanScreen} 
+            options={{ title: 'Scan Barcode' }} 
+          />
+          <Stack.Screen 
+            name="Inventory" 
+            component={InventoryScreen} 
+            options={{ title: 'My Inventory' }} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
